bf63f9c1c141ef820f39e60976048d72ebaa3dc0
wd40bug <wd40bug@gmail.com>
2022-03-14 21:16:15 -0600

    set up basci command line stuff including extraction and reading certain files

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,46 @@
+
+
+use std::io::Read;
+
 use clap::Parser;
-#[derive(Parser)]
-struct Cli{
-    #[clap(parse(from_os_str))]
-    path: std::path::PathBuf,
-}
 fn main() {
-    let args = Cli::parse();
+    let args = cli::Cli::parse();
     let file = std::fs::File::open(&args.path).unwrap();
     let mut archive = zip::ZipArchive::new(file).unwrap();
-    for i in 0..archive.len(){
-        let file = archive.by_index(i).unwrap();
-        println!("{}",file.name());
+    match &args.command{
+        cli::Commands::Read {page: None}=>{
+            
+        },
+        cli::Commands::Read { page: Some(page)}=>{
+            let mut contents = Vec::new();
+            let mut zip_file = archive.by_index(*page).unwrap();
+            zip_file.read_to_end(&mut contents).unwrap();
+            let contents = &contents[..];
+            println!("{}",std::str::from_utf8(contents).unwrap());
+        },
+        cli::Commands::Extract { path }=>{
+            archive.extract(path).unwrap();
+        }
+    }
+}
+pub mod cli{
+    use clap::{Parser, Subcommand};
+    #[derive(Parser)]
+    pub struct Cli{
+        #[clap(subcommand)]
+        pub command: Commands,
+        #[clap(parse(from_os_str))]
+        pub path: std::path::PathBuf,
+    }
+    #[derive(Subcommand)]
+    pub enum Commands{
+        Read{
+            #[clap(short,long)]
+            page: Option<usize>,
+        },
+        Extract{
+            #[clap(parse(from_os_str))]
+            path: std::path::PathBuf,
+        }
     }
 }
