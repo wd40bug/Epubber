4563d04d62309e4140040bdd1d4e5fc5b313c284
bobbobbob15 <wd40bug@gmail.com>
2022-03-16 14:33:17 -0600

    no! I will not write a parser

--- src/main.rs
+++ src/main.rs
@@ -2,45 +2,27 @@
 
 use std::io::Read;
 
-use clap::Parser;
+use clap::StructOpt;
+use cli::cli::{Cli, Commands};
+
 fn main() {
-    let args = cli::Cli::parse();
+    let args = Cli::parse();
     let file = std::fs::File::open(&args.path).unwrap();
     let mut archive = zip::ZipArchive::new(file).unwrap();
     match &args.command{
-        cli::Commands::Read {page: None}=>{
+        Commands::Read {page: None}=>{
             
         },
-        cli::Commands::Read { page: Some(page)}=>{
+        Commands::Read { page: Some(page)}=>{
             let mut contents = Vec::new();
             let mut zip_file = archive.by_index(*page).unwrap();
             zip_file.read_to_end(&mut contents).unwrap();
             let contents = &contents[..];
             println!("{}",std::str::from_utf8(contents).unwrap());
         },
-        cli::Commands::Extract { path }=>{
+        Commands::Extract { path }=>{
             archive.extract(path).unwrap();
         }
     }
 }
-pub mod cli{
-    use clap::{Parser, Subcommand};
-    #[derive(Parser)]
-    pub struct Cli{
-        #[clap(subcommand)]
-        pub command: Commands,
-        #[clap(parse(from_os_str))]
-        pub path: std::path::PathBuf,
-    }
-    #[derive(Subcommand)]
-    pub enum Commands{
-        Read{
-            #[clap(short,long)]
-            page: Option<usize>,
-        },
-        Extract{
-            #[clap(parse(from_os_str))]
-            path: std::path::PathBuf,
-        }
-    }
-}
+
---
+++ src/xml_parser.rs
@@ -0,0 +1,4 @@
+pub fn name_here(content: String){
+    let doc = roxmltree::Document::parse(content).unwrap();
+    
+}
